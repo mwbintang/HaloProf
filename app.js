3c1ca18b2c2bcd2167286aaf768254c6a0ea8632
mwbintang <94796835+mwbintang@users.noreply.github.com>
2022-02-24 12:40:18 +0700

    udah home

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Controller = require('./controllers')
 const doctorRoute = require("./routers/doctorRoute")
 const deseaseRoute = require("./routers/deseaseRoute")
 const registerRoute = require("./routers/registerRoute")
@@ -9,16 +10,13 @@ const userRoute = require("./routers/userRoute")
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:true}));
-// app.get('/', Controller.home)
+app.get('/', Controller.home)
 // app.get('/login')
 // app.get('/logout')
 app.use("/doctor", doctorRoute);
 app.use("/desease", deseaseRoute)
 app.use('/register', registerRoute)
 app.use("/user",userRoute);
-// app.get('/user/:id/', )
-// app.get('/user/:id/checkResult', )
-// app.get('/doctor/:Id', )
 
 
 app.listen(port, ()=>{
